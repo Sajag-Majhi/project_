a015e18626eccc7bef0a81fbfffe09417f7c7e3f
SajagMajhi <sajagmajhi666@gmail.com>
2026-02-16 22:18:57 +0545

    Hari added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Sajag from './Components/Sajag'
+import Hari from './Components/Hari'
 
 function App() {
   return (
     <>
       <Sajag/>
+      <Hari/>
     </>
   )
 }
