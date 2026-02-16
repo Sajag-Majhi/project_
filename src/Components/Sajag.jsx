a6e8c405a4d24f2b6fb1cd8b4336f4203efbfa36
SajagMajhi <sajagmajhi666@gmail.com>
2026-02-16 22:10:50 +0545

    modified

--- src/Components/Sajag.jsx
+++ src/Components/Sajag.jsx
@@ -1,7 +1,7 @@
 export default function Sajag(){
     return(
         <>
-            <h1>Hello world from Sajag Majhi</h1>
+            <h1>Hello world from Sajag Majhi 2</h1>
         </>
     )
 }
