f9a656347e1658b8e2e95a862a846f05c161bf76
Sajag-Majhi <saja649@hcoe.edu.np>
2026-02-16 21:51:16 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Sajag from './Components/Sajag'
+
+function App() {
+  return (
+    <>
+      <Sajag/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Sajag.jsx
@@ -0,0 +1,7 @@
+export default function Sajag(){
+    return(
+        <>
+            <h1>Hello world from Sajag Majhi</h1>
+        </>
+    )
+}
